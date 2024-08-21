164605c136d602bd67549ab62cb6af9e75c9ca8c
Daniyal <bb420f@gmail.com>
2024-08-22 01:45:15 +0500

    new changes done

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 
 
 // element toggle function
-const elementToggleFunc = function(elem) { elem.classList.toggle("active"); }
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
 
 
@@ -12,7 +12,7 @@ const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
 // sidebar toggle functionality for mobile
-sidebarBtn.addEventListener("click", function() { elementToggleFunc(sidebar); });
+sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
 
 
 // custom select variables
@@ -21,11 +21,11 @@ const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
-select.addEventListener("click", function() { elementToggleFunc(this); });
+select.addEventListener("click", function () { elementToggleFunc(this); });
 
 // add event in all select items
 for (let i = 0; i < selectItems.length; i++) {
-  selectItems[i].addEventListener("click", function() {
+  selectItems[i].addEventListener("click", function () {
 
     let selectedValue = this.innerText.toLowerCase();
     selectValue.innerText = this.innerText;
@@ -38,7 +38,7 @@ for (let i = 0; i < selectItems.length; i++) {
 // filter variables
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
-const filterFunc = function(selectedValue) {
+const filterFunc = function (selectedValue) {
 
   for (let i = 0; i < filterItems.length; i++) {
 
@@ -59,7 +59,7 @@ let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
 
-  filterBtn[i].addEventListener("click", function() {
+  filterBtn[i].addEventListener("click", function () {
 
     let selectedValue = this.innerText.toLowerCase();
     selectValue.innerText = this.innerText;
@@ -82,7 +82,7 @@ const formBtn = document.querySelector("[data-form-btn]");
 
 // add event to all form input field
 for (let i = 0; i < formInputs.length; i++) {
-  formInputs[i].addEventListener("input", function() {
+  formInputs[i].addEventListener("input", function () {
 
     // check form validation
     if (form.checkValidity()) {
@@ -102,7 +102,7 @@ const pages = document.querySelectorAll("[data-page]");
 
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function() {
+  navigationLinks[i].addEventListener("click", function () {
 
     for (let i = 0; i < pages.length; i++) {
       if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
@@ -117,5 +117,10 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
   });
 
-  
+
 }
+// Count the number of elements with the class "project-count"
+const projectCount = document.querySelectorAll('.project-count').length;
+
+// Select the element with the class "totoal-project" and update its content
+document.querySelector('.totoal-project').textContent = projectCount;
